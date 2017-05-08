5ec61aa20d25780aa1eb418bb86945afd4e6ebbd
Rama Lanka <rama.lanka@standardbank.co.za>
2017-05-08 17:29:35 +0200

    Design Completed

---
+++ src/main/java/com/wipro/bank/assessment/App.java
@@ -0,0 +1,24 @@
+package com.wipro.bank.assessment;
+
+import com.wipro.bank.entity.*;
+
+import java.util.List;
+
+public class App {
+	
+	public static final List<Account> accounts=null;
+	
+	
+	public static List<Account> getAccounts() {
+		return accounts;
+	}	
+
+	public Account getAccountDetailsByID(int id){
+		return null;
+	}
+	
+	public List<Account> getAccountDetailsByBalance(double amount){
+		return null ;
+		}
+
+}
