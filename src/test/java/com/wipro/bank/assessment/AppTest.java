9f9d450928143b6cf46545aba000a79dc73dc180
Rama Lanka <rama.lanka@standardbank.co.za>
2017-05-08 17:36:43 +0200

    Unit test version updated

---
+++ src/test/java/com/wipro/bank/assessment/AppTest.java
@@ -0,0 +1,19 @@
+package com.wipro.bank.assessment;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void testGetAccountDetailsByID() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAccountDetailsByBalance() {
+		fail("Not yet implemented");
+	}
+
+}
